46c1c42121dd7d88911c2c07a5104dbd12f47344
alexbelenky <145706686+alexbelenky@users.noreply.github.com>
2024-02-16 14:05:38 -0500

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,13 +1,18 @@
 public class Movie {
-    private String title;
-    private String cast;
-    private String director;
-    private String overview;
-    private String runtime;
-    private String userRating;
+    private final String title;
+    private final String cast;
+    private final String director;
+    private final String overview;
+    private final String runtime;
+    private final String userRating;
 
-    public Movie() {
-
+    public Movie(String title, String cast, String director, String overview, String runtime, String userRating) {
+        this.title = title;
+        this.cast = cast.replaceAll("\\|", ", ");
+        this.director = director;
+        this.overview = overview;
+        this.runtime = runtime;
+        this.userRating = userRating;
     }
 
     public String getTitle() {
@@ -30,8 +35,12 @@ public class Movie {
         return runtime;
     }
 
-
     public String getUserRating() {
         return userRating;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Title: %s\nRuntime: %s minutes\nDirected by: %s\nCast: %s\nOverview: %s\nUser rating: %s", title, runtime, director, cast, overview, userRating);
     }
 }
